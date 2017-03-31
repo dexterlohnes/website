d676c95c5177bc411b9ca3cc541713fb102b63ed
Karel Striegel <karel.striegel@ipc.be>
2017-03-31 13:44:28 +0200

    Add private test network config

--- truffle.js
+++ truffle.js
@@ -6,5 +6,10 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+    test: {
+      host: "localhost",
+      port: 8546,
+      network_id: "*" // Match any network id
+    }
   }
 };
