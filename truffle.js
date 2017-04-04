63d390d1c3b41c9d8dfa361d203a6baed555c7e8
jenkins <jenkins@fundrequest.io>
2017-04-04 10:39:26 +0200

    Add more ethereum networks

--- truffle.js
+++ truffle.js
@@ -1,14 +1,24 @@
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
-    development: {
+    sandbox: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     },
-    test: {
+    development: {
       host: "localhost",
       port: 8546,
+      network_id: "*" // Match any network id
+    },
+    private: {
+      host: "localhost",
+      port: 8547,
+      network_id: "*" // Match any network id
+    },
+    main: {
+      host: "localhost",
+      port: 8548,
       network_id: "*" // Match any network id
     }
   }
