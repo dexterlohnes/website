1111e75dcc667cce6b1ae0e2c99a8ecd382c77ee
David Hantson <david.hantson@gmail.com>
2017-04-12 23:14:28 +0200

    Enable auth0 authentication

--- src/modules/home/components/HomePage.js
+++ src/modules/home/components/HomePage.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
+import noop from '../../common/tools/noop'
 
-const HomePage = () => {
+const HomePage = ({
+                      storageValue,
+                      inputValue,
+                      onChangeInputValue = noop,
+                      setStorageValue = noop,
+                      onConnect = noop
+                  }) => {
     return (
         <div>
             <h1>Home</h1>
+            Dont forget to connect
+            <p>The stored value is: {storageValue}</p>
+            <input type="text" value={inputValue} onChange={onChangeInputValue}/>
+            <button onClick={setStorageValue.bind(this,inputValue)}>Set Value</button>
+            <button onClick={onConnect}>Connect</button>
         </div>
-    );
-};
+    )
+}
 
-export default HomePage;
+export default HomePage
---
+++ src/modules/home/containers/LoginPageContainer.js
@@ -0,0 +1,37 @@
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import LoginPage from '../components/LoginPage'
+import AuthService from '../../common/tools/AuthService'
+
+
+export const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+export const mapDispatchToProps = (dispatch) => {
+    // return bindActionCreators({
+    // }, dispatch)
+    return {}
+}
+
+@connect(
+    mapStateToProps,
+    mapDispatchToProps
+)
+export default class LoginPageContainer extends Component {
+    static propTypes = {
+        location: PropTypes.object,
+        auth: PropTypes.instanceOf(AuthService)
+    }
+
+    render() {
+        const { auth } = this.props
+        return (
+            <LoginPage
+                onLogin={auth.login.bind(this)}
+            />
+        )
+    }
+}
