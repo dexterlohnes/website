bf937dc4fc59b65a861f7c20f7e67b2a1e976a8b
David Hantson <david.hantson@gmail.com>
2017-04-26 12:55:24 +0200

    Fix server side rendering

--- server.babel.js
+++ server.babel.js
@@ -13,22 +13,22 @@ app.set('views', path.join(process.cwd(), 'views'));
 app.set('view engine', 'pug');
 
 function renderHtml(req,res) {
-    // renderHTMLString(routes, req, (error, redirectLocation, html) => {
-    //     if (error && error.message === 'Not found') {
-    //         res.status(404).send(error.message);
-    //     }
-        // else if (error) {
-        //     res.status(500).send(error.message);
-        // }
-        // else if (redirectLocation) {
-        //     res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-        // } else {
+    renderHTMLString(routes, req, (error, redirectLocation, html) => {
+        if (error) {
+            if (error.message === 'Not found') {
+                res.status(404).send(error.message);
+            } else {
+                res.status(500).send(error.message);
+            }
+        } else if (redirectLocation) {
+            res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+        } else {
             res.render('index', {
-                // content: html,
+                content: html.content,
                 node_env: process.env.NODE_ENV
-            });
-    //     }
-    // });
+            })
+        }
+    });
 }
 
 app.get('*', assetMiddleware(),(req, res) => {
