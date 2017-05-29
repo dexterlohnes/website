b1a408448559946fa2b24be75670e4390cbebdb6
David Hantson <david.hantson@gmail.com>
2017-05-29 21:32:35 +0200

    Style home introduction

--- src/modules/home/components/HomePage.js
+++ src/modules/home/components/HomePage.js
@@ -4,11 +4,14 @@ import Steps from './Steps'
 import Values from './Values'
 import Goals from './Goals'
 import Team from './Team'
+import {defaultLanguage} from '../../common/tools/Internationalization'
 
-const HomePage = ({}) => {
+const HomePage = ({ language = defaultLanguage }) => {
     return (
         <div className="home">
-            <Introduction />
+            <Introduction
+                language={language}
+            />
             <Steps />
             <Values />
             <Goals />
