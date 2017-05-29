733ee11fd03155c48434309d29852885774615bb
David Hantson <david.hantson@gmail.com>
2017-05-29 22:21:31 +0200

    Add stakeholders

--- src/modules/home/components/Steps.js
+++ src/modules/home/components/Steps.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SectionHeader from './SectionHeader'
-import {Icon} from '../../common/components/Icons'
-import {FormattedMessage} from 'react-intl'
 import WizardStep from './WizardStep'
 
 
