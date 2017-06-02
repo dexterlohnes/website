475535ee4c13aa0566ea0b7bb4d7a72a0d88a455
Karel Striegel <karel.striegel@ipc.be>
2017-06-02 13:25:13 +0200

    Make small txt changes on main page

--- src/locales/en.js
+++ src/locales/en.js
@@ -3,14 +3,14 @@ export default {
     messages: {
         'app.fr.welcome': 'Welcome',
 
-        'app.header.sign-up': 'Sign Up',
+        'app.header.sign-up': 'Get Invite',
         'app.header.crowdsale': 'Crowdsale',
 
         'app.home.introduction.title': 'Funding and rewarding open source development',
         'app.home.introduction.intro': 'FundRequest makes it easy to fund open issues on any project, in any language',
-        'app.home.introduction.get-started': 'Get Started',
+        'app.home.introduction.get-started': 'Coming Soon!',
         'app.home.introduction.already-joined': 'Already joined Fundrequest?',
-        'app.home.introduction.sign-in': 'Sign in',
+        'app.home.introduction.sign-in': 'Get invite',
 
         'app.home.steps.title': 'Getting Started Takes 5 Minutes',
         'app.home.steps.download': 'Download',
