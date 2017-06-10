5999669344a367d816ecc3d8af6d13b95c99311e
David Hantson <david.hantson@gmail.com>
2017-06-10 09:59:21 +0200

    Create responsive menu and add anchor links

--- src/modules/home/components/Team.js
+++ src/modules/home/components/Team.js
@@ -4,118 +4,121 @@ import SectionHeader from './SectionHeader'
 import SectionSubHeader from './SectionSubHeader'
 import Member from './Member'
 import {getPath} from '../../common/tools/URLTools'
+import ScrollableAnchor from 'react-scrollable-anchor'
 
 const Team = ({}) => {
     return (
-        <div className="team section">
-            <div className="content">
-                <div className="gr-container">
-                    <div className="row">
-                        <div className="gr-12 center">
-                            <SectionHeader
-                                title={'app.home.team.title'}
-                            />
+        <ScrollableAnchor id={'about-us'}>
+            <div className="team section">
+                <div className="content">
+                    <div className="gr-container">
+                        <div className="row">
+                            <div className="gr-12 center">
+                                <SectionHeader
+                                    title={'app.home.team.title'}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <br/>
-                    <div className="row">
-                        <div className="gr-12 center">
-                            <SectionSubHeader
-                                title={'app.home.team.core.title'}
-                            />
+                        <br/>
+                        <div className="row">
+                            <div className="gr-12 center">
+                                <SectionSubHeader
+                                    title={'app.home.team.core.title'}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <br/>
-                    <br/>
-                    <div className="row">
+                        <br/>
+                        <br/>
+                        <div className="row">
+                            <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                                <Member
+                                    avatar={getPath('images/app/home/karel-striegel-avatar.jpg')}
+                                    name={'Karel Striegel'}
+                                    title={'app.home.team.karel.title'}
+                                    text={'app.home.team.karel.text'}
+                                />
+                            </div>
+                            <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                                <Member
+                                    avatar={getPath('images/app/home/tim-dierckxsens-avatar.jpg')}
+                                    name={'Tim Dierckxsens'}
+                                    title={'app.home.team.tim.title'}
+                                    text={'app.home.team.tim.text'}
+                                />
+                            </div>
+                            <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                                <Member
+                                    avatar={getPath('images/app/home/davy-van-roy-avatar.jpg')}
+                                    name={'Davy Van Roy'}
+                                    title={'app.home.team.davy.title'}
+                                    text={'app.home.team.davy.text'}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                                <Member
+                                    avatar={getPath('images/app/home/david-hantson-avatar.png')}
+                                    name={'David Hantson'}
+                                    title={'app.home.team.david.title'}
+                                    text={'app.home.team.david.text'}
+                                />
+                            </div>
+                            <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                                <Member
+                                    avatar={getPath('images/app/home/quinten-de-swaef-avatar.jpg')}
+                                    name={'Quinten De Swaef'}
+                                    title={'app.home.team.quinten.title'}
+                                    text={'app.home.team.quinten.text'}
+                                />
+                            </div>
+                            <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                                <Member
+                                    avatar={getPath('images/app/home/tom-degroote-avatar.jpg')}
+                                    name={'Tom Degroote'}
+                                    title={'app.home.team.tom.title'}
+                                    text={'app.home.team.tom.text'}
+                                />
+                            </div>
+                        </div>
+                        <br/>
+                        <div className="row">
+                            <div className="gr-12 center">
+                                <SectionSubHeader
+                                    title={'app.home.team.advisors.title'}
+                                />
+                            </div>
+                        </div>
+                        <br/>
+                        <br/>
                         <div className="gr-4 center gr-12@mobile gr-6@tablet">
                             <Member
-                                avatar={getPath('images/app/home/karel-striegel-avatar.jpg')}
-                                name={'Karel Striegel'}
-                                title={'app.home.team.karel.title'}
-                                text={'app.home.team.karel.text'}
+                                avatar={getPath('images/app/home/thomas-matthys-avatar.jpg')}
+                                name={'Thomas Matthys'}
+                                title={'app.home.team.thomas.title'}
+                                text={'app.home.team.thomas.text'}
                             />
                         </div>
                         <div className="gr-4 center gr-12@mobile gr-6@tablet">
                             <Member
-                                avatar={getPath('images/app/home/tim-dierckxsens-avatar.jpg')}
-                                name={'Tim Dierckxsens'}
-                                title={'app.home.team.tim.title'}
-                                text={'app.home.team.tim.text'}
+                                avatar={getPath('images/app/home/dndfisc-avatar.jpg')}
+                                name={'D&D Fisc'}
+                                title={'app.home.team.dndfisc.title'}
+                                text={'app.home.team.dndfisc.text'}
                             />
                         </div>
                         <div className="gr-4 center gr-12@mobile gr-6@tablet">
                             <Member
-                                avatar={getPath('images/app/home/davy-van-roy-avatar.jpg')}
-                                name={'Davy Van Roy'}
-                                title={'app.home.team.davy.title'}
-                                text={'app.home.team.davy.text'}
+                                avatar={getPath('images/app/home/portelio-avatar.jpg')}
+                                name={'Portelio'}
+                                title={'app.home.team.portelio.title'}
+                                text={'app.home.team.portelio.text'}
                             />
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="gr-4 center gr-12@mobile gr-6@tablet">
-                            <Member
-                                avatar={getPath('images/app/home/david-hantson-avatar.png')}
-                                name={'David Hantson'}
-                                title={'app.home.team.david.title'}
-                                text={'app.home.team.david.text'}
-                            />
-                        </div>
-                        <div className="gr-4 center gr-12@mobile gr-6@tablet">
-                            <Member
-                                avatar={getPath('images/app/home/quinten-de-swaef-avatar.jpg')}
-                                name={'Quinten De Swaef'}
-                                title={'app.home.team.quinten.title'}
-                                text={'app.home.team.quinten.text'}
-                            />
-                        </div>
-                        <div className="gr-4 center gr-12@mobile gr-6@tablet">
-                            <Member
-                                avatar={getPath('images/app/home/tom-degroote-avatar.jpg')}
-                                name={'Tom Degroote'}
-                                title={'app.home.team.tom.title'}
-                                text={'app.home.team.tom.text'}
-                            />
-                        </div>
-                    </div>
-                    <br/>
-                    <div className="row">
-                        <div className="gr-12 center">
-                            <SectionSubHeader
-                                title={'app.home.team.advisors.title'}
-                            />
-                        </div>
-                    </div>
-                    <br/>
-                    <br/>
-                    <div className="gr-4 center gr-12@mobile gr-6@tablet">
-                        <Member
-                            avatar={getPath('images/app/home/thomas-matthys-avatar.jpg')}
-                            name={'Thomas Matthys'}
-                            title={'app.home.team.thomas.title'}
-                            text={'app.home.team.thomas.text'}
-                        />
-                    </div>
-                    <div className="gr-4 center gr-12@mobile gr-6@tablet">
-                        <Member
-                            avatar={getPath('images/app/home/dndfisc-avatar.jpg')}
-                            name={'D&D Fisc'}
-                            title={'app.home.team.dndfisc.title'}
-                            text={'app.home.team.dndfisc.text'}
-                        />
-                    </div>
-                    <div className="gr-4 center gr-12@mobile gr-6@tablet">
-                        <Member
-                            avatar={getPath('images/app/home/portelio-avatar.jpg')}
-                            name={'Portelio'}
-                            title={'app.home.team.portelio.title'}
-                            text={'app.home.team.portelio.text'}
-                        />
                     </div>
                 </div>
             </div>
-        </div>
+        </ScrollableAnchor>
     )
 }
 
