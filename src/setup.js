358a7b7881a6b5b3872122a253b511861a9b7c5c
David Hantson <david.hantson@gmail.com>
2017-06-21 12:12:35 +0200

    Make header only full white when scrolling

--- src/setup.js
+++ src/setup.js
@@ -1,10 +1,36 @@
 import axios from 'axios'
 import {prepareLocaleData} from './modules/common/tools/Internationalization'
 
+const styleFixedNavigationOnScroll = () => {
+    if(typeof window !== 'undefined') {
+        let fixedHeaderClass = 'header-fixed'
+
+        window.addEventListener('DOMContentLoaded', () => {
+            window.addEventListener('scroll', (event) => {
+                let scrollTop = document.body.scrollTop
+                let header = document.getElementsByClassName('header')[0]
+
+                if (scrollTop > 1) {
+                    header.className += ' ' + fixedHeaderClass
+                }
+                if (scrollTop < 1) {
+                    let cssClass = ' ' + header.className + ' '
+                    let index = cssClass.indexOf(' ' + fixedHeaderClass + ' ')
+                    if (index >= 0) {
+                        header.className = cssClass.substr(0, index) + ' ' + cssClass.substr(index + fixedHeaderClass.length + 1)
+                    }
+                    header.classList.remove('header-fixed')
+                }
+            })
+        })
+    }
+}
+
 export default () => {
     // axios.defaults.auth = {
     //     username: 'panel-management',
     //     password: 'yHA2qjNbr2MrV6s'
     // };
     prepareLocaleData()
+    styleFixedNavigationOnScroll()
 }
