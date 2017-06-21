4ce7008762d8c9cf5831c6cb16497643f1115e8b
David Hantson <david.hantson@gmail.com>
2017-06-21 12:29:40 +0200

    Fix bug in scroll header class

--- src/setup.js
+++ src/setup.js
@@ -10,16 +10,15 @@ const styleFixedNavigationOnScroll = () => {
                 let scrollTop = document.body.scrollTop
                 let header = document.getElementsByClassName('header')[0]
 
-                if (scrollTop > 1) {
+                if (scrollTop > 1 && header.className.indexOf(fixedHeaderClass) === -1) {
                     header.className += ' ' + fixedHeaderClass
                 }
                 if (scrollTop < 1) {
-                    let cssClass = ' ' + header.className + ' '
-                    let index = cssClass.indexOf(' ' + fixedHeaderClass + ' ')
+                    let cssClass = header.className
+                    let index = cssClass.indexOf(' ' + fixedHeaderClass)
                     if (index >= 0) {
-                        header.className = cssClass.substr(0, index) + ' ' + cssClass.substr(index + fixedHeaderClass.length + 1)
+                        header.className = cssClass.substr(0, index) + cssClass.substr(index + fixedHeaderClass.length + 1)
                     }
-                    header.classList.remove('header-fixed')
                 }
             })
         })
