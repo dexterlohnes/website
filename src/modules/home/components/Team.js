dfb390ef79736f2a0be1a474435e3f5a04e45429
Karel Striegel <karel.striegel@ipc.be>
2017-06-30 15:33:20 +0200

    Nakrin Narula as  Investment & Growth advisor

--- src/modules/home/components/Team.js
+++ src/modules/home/components/Team.js
@@ -97,7 +97,7 @@ const Team = ({}) => {
                         </div>
                         <br/>
                         <br/>
-                        <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                        <div className="gr-6 center gr-12@mobile gr-6@tablet">
                             <Member
                                 avatar={getPath('images/app/home/thomas-matthys-avatar.jpg')}
                                 name={'Thomas Matthys'}
@@ -105,7 +105,15 @@ const Team = ({}) => {
                                 text={'app.home.team.thomas.text'}
                             />
                         </div>
-                        <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                        <div className="gr-6 center gr-12@mobile gr-6@tablet">
+                            <Member
+                                avatar={getPath('images/app/home/nakrin-narula-avatar.jpg')}
+                                name={'Nakrin Narula'}
+                                title={'app.home.team.nakrin.title'}
+                                text={'app.home.team.nakrin.text'}
+                            />
+                        </div>
+                        <div className="gr-6 center gr-12@mobile gr-6@tablet">
                             <Member
                                 avatar={getPath('images/app/home/dndfisc-avatar.jpg')}
                                 name={'D&D Fisc'}
@@ -113,7 +121,7 @@ const Team = ({}) => {
                                 text={'app.home.team.dndfisc.text'}
                             />
                         </div>
-                        <div className="gr-4 center gr-12@mobile gr-6@tablet">
+                        <div className="gr-6 center gr-12@mobile gr-6@tablet">
                             <Member
                                 avatar={getPath('images/app/home/portelio-avatar.jpg')}
                                 name={'Portelio'}
