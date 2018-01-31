6175061bafd388f4cd77741fa2d067c680da9102
bongo <bongo@outlook.com>
2018-01-31 10:01:36 +0100

    #4 #5

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,11 @@ $(document).ready( function() {
 
 	var navigation = document.querySelector(".headroom");
 	var headroom = new Headroom(navigation, {
-	  "tolerance": 10
+	  tolerance: 10,
+	  classes : {
+	  	pinned: "slideDown",
+      	unpinned: "slideUp"
+      }
 	});
     headroom.init();
     
