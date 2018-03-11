713f5630fc41ce119bc5080f26af320958d55cf1
Steve Maris <steve_maris@hotmail.com>
2018-03-11 01:28:28 +0100

    - Remove css from agency - Move to bootstrap 4 - Update og:* and twitter:* - Update literals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,155 +1,45 @@
-var curirspinner;
-
-$(document).ready( function() {
-    // console.log('Ready');
-
-	var navigation = document.querySelector(".headroom");
-	var headroom = new Headroom(navigation, {
-	  tolerance: 10,
-	  classes : {
-	  	pinned: "slideDown",
-      	unpinned: "slideUp"
-      }
-	});
+$(document).ready(function() {
+    var navigation = document.querySelector(".fnd-header");
+    var headroom = new Headroom(navigation, {
+        tolerance: 5,
+        classes: {
+            top: "navbar-dark",
+            notTop: "navbar-light"
+        }
+    });
     headroom.init();
-    
+
     $('.js_horizontal').lightSlider({
-        autoWidth:true,
-        loop:false,
+        autoWidth: true,
+        loop: false,
         controls: false,
-        item:5,
+        item: 5,
         slideMargin: 25,
         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        responsive : [
+        responsive: [
             {
-                breakpoint:991,
+                breakpoint: 991,
                 settings: {
-                    item:4,
+                    item: 4,
                     slideMargin: 10,
-                    slideMove:1
-                  }
+                    slideMove: 1
+                }
             },
             {
-                breakpoint:767,
+                breakpoint: 767,
                 settings: {
-                    item:2,
+                    item: 2,
                     slideMargin: 0,
-                    slideMove:1
+                    slideMove: 1
                 }
             }
         ]
     });
 
-    $('.vertical-slider').lightSlider({
-        loop:false,
-        keyPress:true,
-        item: 1,
-        adaptiveHeight: true,
-        mode: 'fade',
-        onSliderLoad: function(_el) 
-        {
-        	var slidecontainer = $(_el).closest(".lSSlideOuter");
-        	slidecontainer.find(".lSPager li").each(function(idx) {
-        		if( slidecontainer.find(".lslide:eq("+idx+")").attr("data-year") )
-        		{
-        			$(this).find("a").text(slidecontainer.find(".lslide:eq("+idx+")").attr("data-year"));
-        		}
-        		else
-        		{
-        			$(this).attr("data-title", slidecontainer.find(".lslide:eq("+idx+")").attr("data-title"));
-        		}
-        	});
-        	
-        	if(slidecontainer.find(".monthslider").length > 0)
-        		slidecontainer.append("<div class='monthselector'></div>");
-
-
-        	var slidecontainer = $(_el).closest(".lSSlideOuter");
-        	/*var currentmonthitem = slidecontainer.find(".monthslider:eq(" + (_el.getCurrentSlideCount() -1) + ")")
-    		var submonths = [];
-    		$(currentmonthitem).find(".month").each(function() {
-    			submonths.push($(this).attr("data-month"));
-    		});
-    		$(".monthselector").html("");
-    		curirspinner = $(".monthselector").irSpinner({
-				list: submonths,
-				vertical: ($(window).width() < 991  ? false : true)
-			});*/
-        },
-        onAfterSlide: function(_el)
-        {
-        	/*var slidecontainer = $(_el).closest(".lSSlideOuter");
-        	var currentmonthitem = slidecontainer.find(".monthslider:eq(" + (_el.getCurrentSlideCount() -1) + ")")
-    		var submonths = [];
-    		$(currentmonthitem).find(".month").each(function() {
-    			submonths.push($(this).attr("data-month"));
-    		});
-    		if(submonths.length > 0)
-    		{
-	    		$(".monthselector").html("");
-	    		curirspinner = $(".monthselector").irSpinner({
-					list: submonths,
-					vertical: ($(window).width() < 991  ? false : true)
-				});
-    		}*/
-        }
-    });
-
     $(".scrolltonext").on("click", function() {
-		$('html, body').animate({
-       		scrollTop: $(this).closest('section').next().offset().top
-    	}, 800);
-    	return false;
-    });
-
-    $(function() {
-        $('.memberinfo').matchHeight({
-            byRow: false,
-        });
-
-        $('section.steps .lslide ').matchHeight({
-            byRow: false,
-        });
-
-    });
-
-    $('.navlinks a[href*="#"]').on('click', function (e) {
-        nav = $('.navlinks');
-        e.preventDefault();
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        // nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-        $(this).addClass('active');
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500, 'linear');
+            scrollTop: $(this).closest('section').next().offset().top
+        }, 800);
+        return false;
     });
-
-    // vertical align image -> header
-    var rowh = $('.js_row').height();
-    $('.centerdiv').css('min-height', rowh+'px'); 
-
-
-    //var test = $('.lslide').attr("data-title");
-    $(".lslide").change(function () {
-       var test = $(this).data("data-title");
-        console.log(test);
-    });
-    
-    window.onscroll = function (e) {
-        if ($(window).width() < 768) {
-            if ($( 'div.collapse ' ).hasClass("in")) {
-                $('div.collapse').removeClass("in"); 
-            }
-        }
-    }
-
 });
-
-function fund_updatemonth(idx)
-{
-	$(".monthslider .month").hide();
-	$(".monthslider").find(".month:eq(" +idx+ ")").show();
-	$( window ).trigger('refreshheight');
-}
